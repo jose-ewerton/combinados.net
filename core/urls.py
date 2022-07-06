11809c8b95ccd1c08dbd0faf383515e89d0346d1
jose-ewerton <jose.ewerton@escolar.ifrn.edu.br>
2022-07-06 14:48:01 -0300

    att pós à aula

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, home
+
+'''
+from django.http import HttpResponse
+
+def my_view(request):
+    return HttpResponse(" Alguma coisa")
+'''
 
 urlpatterns = [
+    #(caminho, view.py )
+    #path('sobre/', my_view),
     path('', HomeView.as_view(), name = 'home'),
+    path('home/', home, name="home-2"),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+    
+
+def home(request):
+    return render(request, "home2.html")
