cda5bb731d2a23483def0fa1a539eae6afaef774
Jose Ewerton da Cruz de Souza <jose.ewerton@escolar.ifrn.edu.br>
2022-07-14 11:55:00 -0300

    Att do CA

--- base/views.py
+++ base/views.py
@@ -1,13 +1,9 @@
-#from django.shortcuts import render
-from django.views.generic import TemplateView
-# Create your views here.
-'''
-context = {
-    'name' = 'Ewerton',
-}
 
-def home(request):
-    return render('home.html')
-'''
+from django.views.generic import TemplateView
+
+
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
