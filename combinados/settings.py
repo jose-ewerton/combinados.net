25d3e09947c3388f944fccde267fcc6678d5bb83
Jose Ewerton da Cruz de Souza <jose.ewerton@escolar.ifrn.edu.br>
2022-08-09 20:10:17 -0300

    Att setting for images

--- combinados/settings.py
+++ combinados/settings.py
@@ -133,6 +133,11 @@ STATICFILES_DIRS = [
     #'/var/www/static/',
 ]
 
+MEDIA_URL = '/img/'
+MEDIA_ROOT = BASE_DIR/'static/img/'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
